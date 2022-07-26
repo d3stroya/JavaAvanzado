faf56bcd9a75b90fc46fa8d1d51aafce5e27199e
d3stroya <107263982+d3stroya@users.noreply.github.com>
2022-07-26 12:20:08 +0200

    ejercicio14

---
+++ ejercicio14/Precio.java
@@ -0,0 +1,9 @@
+package creacionales.factory;
+
+/**
+ * Interfaz que establece el método getPrecio
+ */
+
+public interface Precio {
+    double getPrecio();
+}
---
+++ ejercicio14/PrecioGBP.java
@@ -0,0 +1,10 @@
+package creacionales.factory;
+/**
+ * Clase que implementa la interfaz precio cuando este está en GBP.
+ */
+public class PrecioGBP implements Precio {
+    @Override
+    public double getPrecio() {
+        return 2.1;
+    }
+}
